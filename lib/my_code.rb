e264cba673aee8ee1d3e258b02ef8617bf26299c
Catherine Yang <liahnee@gmail.com>
2019-07-19 21:36:12 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,11 +11,15 @@ end
 def reduce(source_array, starting_point = 0)
   new = starting_point
   source_array.length.times {|val|
+<<<<<<< HEAD
     if  !!yield(new, source_array[val])
       new = yield(source_array[val], new)
     else 
       new = FALSE
     end
+=======
+    new = yield(source_array[val], new)
+>>>>>>> bd6a6c1b4a4638d679fac62be5a5d55669fdf69a
   }
   return new
 end
