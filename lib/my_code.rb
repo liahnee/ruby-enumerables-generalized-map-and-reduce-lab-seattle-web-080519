310ea8546f136c8c9557ac6b836e3b6a9023e450
Catherine Yang <liahnee@gmail.com>
2019-07-19 07:41:21 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,21 @@
 # Your Code Here
+
+def map(array)
+  new = []
+  array.length.times {|n|
+    new[n] = yield(array[n])
+  }
+  return new
+end
+
+def reduce(source_array, starting_point = 0)
+  new = starting_point
+  source_array.length.times {|val|
+    if  !!yield(new, source_array[val])
+      new = yield(source_array[val], new)
+    else 
+      new = FALSE
+    end
+  }
+  return new
+end
